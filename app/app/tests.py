e49c4911f2cc76da7c76ea46f48f01d09e41ccff
JiaanZZZ <jiaanzhu0618@gmail.com>
2022-09-07 12:48:18 +0100

    update

--- app/app/tests.py
+++ app/app/tests.py
@@ -4,10 +4,9 @@ from app import calc
 
 class calcTests(SimpleTestCase):
     def test_add_numbers(self):
-        res=calc.add(5,6)
-        self.assertEqual(res,11)
-
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-        res=calc.subtract(5,6)
-        self.assertEqual(res,-1)
+        res = calc.subtract(5, 6)
+        self.assertEqual(res, -1)
