a14c64cff05e489f6bfb8f3bb1af3e543befebfe
JiaanZZZ <jiaanzhu0618@gmail.com>
2022-09-20 23:09:21 +0100

    updated

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,6 +1,7 @@
 """
 Serializers for recipe APIs
 """
+from app.core.models import Tag
 from rest_framework import serializers
 
 from core.models import Recipe
@@ -17,3 +18,10 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view."""
     class Meta(RecipeSerializer.Meta):
         fields=RecipeSerializer.Meta.fields+['description']
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tags"""
+    class Meta:
+        model = Tag
+        fields=['id','name']
+        read_only_fields = ['id']
